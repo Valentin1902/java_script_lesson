b6981a841c5be827917d473dc221574237c72e05
Valentin1902 <ar4i373@ukr.net>
2024-07-06 17:52:04 +0300

    first commit

---
+++ js/lesson.js
@@ -0,0 +1,68 @@
+
+// const age = 18;
+// const hasLicense = true;
+// const isWeekend = true;
+// const isDrunk = true;
+
+// // if (age >= 18 && hasLicense && isWeekend && !isDrunk){
+// //     console.log("Поїхали");
+
+// // }else{
+// //     console.log("не можна");
+// // }
+
+// // age >= 18 && hasLicense && isWeekend && !isDrunk ? console.log("Поїхали") : console.log("не можна");
+
+// if(age >= 18 && hasLicense && isWeekend && isDrunk){
+// if(age >= 18){
+//     console.log("Дорослий");
+
+// }else if(hasLicense ){
+//     console.log("Вміє їздити")
+// }else if(isWeekend){
+// console.log("Друже вихідний!");
+// }else if(isDrunk){
+//     console.log("не тверезий!");
+// }
+// }else{
+//     console.log("block");
+// }
+
+
+// // Цикли 
+
+// for (let i = 0; i <= 5; i++){
+// console.log(i);
+// };
+
+// let value 
+// const numbers = [1,2,3,4,5,6];
+// for (const number of numbers){
+// console.log(number);
+// value = number
+// }
+// let i = 0;
+// while (i < numbers.length - 1) {
+  
+//     console.log(numbers[i]);
+// }
+
+
+// const {btn:[btn_en, btn_ua, btn_fr]} = reps;
+
+// console.dir(btn_ua);
+
+// const LangBox = document.querySelector(".lang_box");
+// LangBox.innerHTML = "";
+// const newElement = `<div></div>`
+// LangBox.insertAdjacentHTML("afterbegin", newElement);
+// const newElement2 = document.createElement('div');
+// LangBox.insertAdjacentElement('dsadas', newElement2);
+
+
+
+
+// const text = document.querySelector('.h3');
+// window.addEventListener("scroll", (e) => {
+
+// })
